90c5bdaa464eeb2d114d2ca9a32bfd9b9f423722
Michael Crawford <crawfordwebdev@users.noreply.github.com>
2022-07-14 14:37:16 -0600

    adds drink schema

---
+++ models/drink.js
@@ -0,0 +1,20 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const drinkSchema = new mongoose.Schema({
+  name: String,
+  imageURL: String,
+  owner: { type: mongoose.Schema.Types.ObjectId, ref: 'Profile' },
+  isAlcoholic: Boolean,
+  ingredients: {type: mongoose.Schema.Types.ObjectId, ref: "Ingredient"},
+  reviews: {type: mongoose.Schema.Types.ObjectId, ref: "Review"}
+}, {
+  timestamps: true
+})
+
+const Drink = mongoose.model('Drink', drinkSchema)
+
+export {
+  Drink
+}
