f67b5688a9cdc08f9d3065a2dd242aa4c6c7921d
Michael Crawford <crawfordwebdev@users.noreply.github.com>
2022-07-15 07:21:53 -0600

    adds js extension to controllers

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import * as reviewsCtrl from '../controllers/reviews'
+import * as reviewsCtrl from '../controllers/reviews.js'
 import { decodeUserFromToken, checkAuth } from '../middleware/auth.js'
 
 const router = Router()
