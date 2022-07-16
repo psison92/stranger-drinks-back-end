6ade401e75ed196d4f3d946b2e939b1e39dd9989
Michael Crawford <crawfordwebdev@users.noreply.github.com>
2022-07-16 13:06:24 -0600

    adds measurement as subdocument to drink

--- models/drink.js
+++ models/drink.js
@@ -2,14 +2,26 @@ import mongoose from 'mongoose'
 
 const Schema = mongoose.Schema
 
-const drinkSchema = new mongoose.Schema({
+const measurementSchema = new Schema({
+  quantity: {type: Number, required: true},
+  unit: {type: String, required: true},
+  ingredient: {
+    type: Schema.Types.ObjectId, 
+    ref: "Ingredient", 
+    required: true
+  },
+}, {
+  timestamps: true
+})
+
+const drinkSchema = new Schema({
   name: { type: String, required: true},
   alternateName: String,
   imageURL: String,
-  owner: { type: mongoose.Schema.Types.ObjectId, ref: 'Profile' },
+  owner: { type: Schema.Types.ObjectId, ref: 'Profile' },
   isAlcoholic: Boolean,
-  recipe: [{type: mongoose.Schema.Types.ObjectId, ref: "Measurement"}],
-  reviews: [{type: mongoose.Schema.Types.ObjectId, ref: "Review"}]
+  recipe: [measurementSchema],
+  reviews: [{type: Schema.Types.ObjectId, ref: "Review"}]
 }, {
   timestamps: true
 })
